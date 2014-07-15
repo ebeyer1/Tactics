a5d2f95f8b75bc2c41f881a58c36006bc9d7fbb2
Eric Beyer <eric@Erics-MacBook-Pro.local>
2014-07-15 18:50:25 -0400

    Removing commented out code

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class Grid : MonoBehaviour {
-	//public Player player;
 	public GameObject plane;
 	public int width = 10;
 	public int height = 10;
@@ -28,29 +27,5 @@ public class Grid : MonoBehaviour {
 		staticPlayer.transform.position = new Vector3 (3, 0, 3); // 0,0,0
 		staticPlayer.renderer.material.color = Color.black;
 		staticPlayer.AddComponent ("Player");
-	}
-
-	void OnGUI() {
-		//if (GUI.Button (new Rect (10, 10, 150, 100), " Delete grid [3,3]")) {
-			//Destroy(grid[3,3]);
-			// turn it yellow
-			//MyGrid[3,3].renderer.material.color = Color.yellow;
-
-			// highlight all around it
-		//}
-	}
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnMouseDown() {
-		Debug.Log ("clicked");
 	}
 }
