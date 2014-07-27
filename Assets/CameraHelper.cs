cdb966762cdd949077f732f63cb3f55ab8019ea6
Eric Beyer <eric@Erics-MacBook-Pro.local>
2014-07-27 01:12:36 -0400

    Adding camera helper to change game view

---
+++ Assets/CameraHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHelper : MonoBehaviour {
+	private float rotationSpeed = .15f;
+	private float minZoom = 9.5f;
+	private float maxZoom = 14f;
+	
+	void Update () {
+		var currentPos = transform.position;
+		if (Input.GetAxis ("Vertical") > 0) { // up arrow
+			var newY = currentPos.y - rotationSpeed;
+			newY = newY <= minZoom ? minZoom : newY;
+			transform.position = new Vector3(currentPos.x, (float)newY, currentPos.z);
+		}
+		if (Input.GetAxis ("Vertical") < 0) { // down arrow
+			var newY = currentPos.y + rotationSpeed;
+			newY = newY >= maxZoom ? maxZoom : newY;
+			transform.position = new Vector3(currentPos.x, (float)newY, currentPos.z);
+		}
+	}
+}
