1067df9d8d2a3b945320642821fa9b7e08be9eb6
Eric Beyer <eric@Erics-MacBook-Pro.local>
2014-07-27 12:12:56 -0400

    Updating zoom

--- Assets/CameraHelper.cs
+++ Assets/CameraHelper.cs
@@ -2,21 +2,33 @@
 using System.Collections;
 
 public class CameraHelper : MonoBehaviour {
-	private float rotationSpeed = .15f;
-	private float minZoom = 9.5f;
-	private float maxZoom = 14f;
+	private static float rotationRate = .021f;
+	
+	private static float minZoomY = 7f;
+	private static float maxZoomY = 14f;
+	private static float minZoomZ = -3f;
+	private static float maxZoomZ = 0f;
+	
+	private float ySpeed = (maxZoomY - minZoomY) * rotationRate;
+	private float zSpeed = (maxZoomZ - minZoomZ) * rotationRate;
 	
 	void Update () {
 		var currentPos = transform.position;
 		if (Input.GetAxis ("Vertical") > 0) { // up arrow
-			var newY = currentPos.y - rotationSpeed;
-			newY = newY <= minZoom ? minZoom : newY;
-			transform.position = new Vector3(currentPos.x, (float)newY, currentPos.z);
+			var newY = currentPos.y - ySpeed;
+			newY = newY <= minZoomY ? minZoomY : newY;
+
+			var newZ = currentPos.z + zSpeed;
+			newZ = newZ >= maxZoomZ ? maxZoomZ : newZ;
+			transform.position = new Vector3(currentPos.x, (float)newY, (float)newZ);
 		}
 		if (Input.GetAxis ("Vertical") < 0) { // down arrow
-			var newY = currentPos.y + rotationSpeed;
-			newY = newY >= maxZoom ? maxZoom : newY;
-			transform.position = new Vector3(currentPos.x, (float)newY, currentPos.z);
+			var newY = currentPos.y + ySpeed;
+			newY = newY >= maxZoomY ? maxZoomY : newY;
+
+			var newZ = currentPos.z - zSpeed;
+			newZ = newZ <= minZoomZ ? minZoomZ : newZ;
+			transform.position = new Vector3(currentPos.x, (float)newY, (float)newZ);
 		}
 	}
 }
